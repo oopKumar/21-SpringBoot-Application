3b7ab89f88bf957f311d34b02a069a1dd2369f13
oopKumar <117702232+oopKumar@users.noreply.github.com>
2022-11-20 11:06:19 +0530

    Update Application.java

--- src/main/java/com/oop/Application.java
+++ src/main/java/com/oop/Application.java
@@ -11,11 +11,12 @@ public class Application {
 		 ConfigurableApplicationContext run =SpringApplication.run(Application.class, args);
 		 //HIS-134
 		 //left HIS-122 at middle and working on HIS-134
+		int i =10;
 		 run.close();
 	}
 	//His-200 related changes
 	public void doProcess() {
-
+	
 		//logic goes here
 		}
 
